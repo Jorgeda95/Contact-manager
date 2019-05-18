214e2ce5c0fbc46781b14bd53ab3cdb525752332
Jorgeda95 <jorgepineda@ufm.edu>
2019-05-18 11:42:13 -0600

    Update pruebas.py
    Pruebas Post

--- pruebas.py
+++ pruebas.py
@@ -1,8 +1,19 @@
 
-
 import requests
 
+#UrlPost="https://reqres.in/api/users"
+#Metodo 1 Si desea que la url este ingresada pero solo se desea cambiar una vez
 
+UrlPost=input("Ingrese la url con la desea utilizar el metodo POST:\n  ")
+#Metodo 2 Si desea ingresar la url manualmente    
+payload={'FirstName': 'Michael', 'LastName': 'Kirk', 'phone': '123123'}
+
+r=requests.post(UrlPost, json=payload)
+print(r)
+print(r.text)
+
+
+'''
 #UrlConsulta=input("Ingrese la Url que desea utilizar:  ")
 #UrlConsulta="http://demo7862839.mockable.io/contacts?gid=100"
 url="http://demo7862839.mockable.io/contacts?gid=100"
@@ -24,13 +35,11 @@ data_get = get_response.json()
 
 r = requests.get(url)
 print(r.json())
-#escribe 
-
 '''
 
 
-diccionarioMaestro = {
-    'stevenwilson': {'nombre': "Steven",'apellido': "Wilson", 'telefono': "45656765"},
-    'davidcorzo': {'nombre': 'David', 'apellido': 'Corzo', 'telefono': '30177050'}
 
-'''
+#diccionarioMaestro = {
+ #   'stevenwilson': {'nombre': "Steven",'apellido': "Wilson", 'telefono': "45656765"},
+  #  'davidcorzo': {'nombre': 'David', 'apellido': 'Corzo', 'telefono': '30177050'}
+
