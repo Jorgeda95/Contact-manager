51a8b51bf0fc71da58c7a59dbc5bd9a258a5dd56
Jorgeda95 <jorgepineda@ufm.edu>
2019-05-18 10:42:00 -0600

    Update pruebas.py
    pruba diccionario

--- pruebas.py
+++ pruebas.py
@@ -1,76 +1,36 @@
+
+
 import requests
 
-UrlConsulta="http://demo7862839.mockable.io/contacts?gid=100"
+
+#UrlConsulta=input("Ingrese la Url que desea utilizar:  ")
+#UrlConsulta="http://demo7862839.mockable.io/contacts?gid=100"
+url="http://demo7862839.mockable.io/contacts?gid=100"
+
 
 UrlGet="http://demo7862839.mockable.io/contacts?gid=100"
 
-#Metodo Get
+
+
 gid = input("Ingrese el gid ")
 
 params = {'gid':gid}
 
-r = requests.get(UrlConsulta1)
+contact_list = [{'FirstName': 'Michael', 'LastName': 'Kirk', 'phone': '123123'}]
+
+get_response =requests.get(url = url,params =params)
+
+data_get = get_response.json()
+
+r = requests.get(url)
 print(r.json())
 
 
-
-
-
-
 '''
-# diccionarioMaestro = {}
-# def addContacts(nombre, apellido, telefono):
-#     key=str(nombre) + str(apellido)
-#     nestedDictionary = {'nombre':nombre,'apellido':apellido,'telefono':telefono}
-#     diccionarioMaestro[key] =[nestedDictionary]
-#     print(diccionarioMaestro)
 
-# nombre = input('NAME: ')
-# apellido = input('APELLIDO')
-# telefono = input(str('telefono'))
-
-# addContacts(nombre,apellido,telefono)
 
 diccionarioMaestro = {
     'stevenwilson': {'nombre': "Steven",'apellido': "Wilson", 'telefono': "45656765"},
     'davidcorzo': {'nombre': 'David', 'apellido': 'Corzo', 'telefono': '30177050'}
 
-# remove = input(str("Ingrese el contacto a eliminar: → ")) davidcorzo
-# remove = remove.lower
-
-}
-def produceContactID(nombre,apellido):
-    nombre = nombre.lower()
-    apellido = apellido.lower()
-    key=str(nombre) + str(apellido)
-    return key
-
-nombre = input('NOMBRE del contacto que desea borrar: ')
-apellido = input('APELLIDO del contacto que desea borrar: ')
-
-
-def removeContact(nombre,apellido):
-    key = produceContactID(nombre,apellido)
-    confirmation = input("Estás seguro que quieres borrar el contacto → {} {} ← con el número de → telefono ← {} ← (Ingrese si o no) → ".format(diccionarioMaestro[key]['nombre'],diccionarioMaestro[key]['apellido'],diccionarioMaestro[key]['telefono']))
-    if confirmation == "si":
-        try:
-            del diccionarioMaestro[key]
-        except TypeError:
-            print("El contacto ingresado está mal escrito o no existe en la lista de contactos")
-        except KeyError:
-            print("El contacto ingresado está mal escrito o no existe en la lista de contactos")
-    else: 
-        print("No se eliminó el contacto") 
-
-removeContact(nombre,apellido)
-print(diccionarioMaestro)
-
-
-
-#fase 6 
-esto es una prueba para las branch
 '''
-
-
-
-
