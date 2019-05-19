a4d39a6e7f1ba1f9d30273cebc38efaa504ea661
Jorgeda95 <jorgepineda@ufm.edu>
2019-05-19 00:49:15 -0600

    Update funcionesContactManager.py
    Fase 6 con posibles mejoras que se pueden hacer

--- funcionesContactManager.py
+++ funcionesContactManager.py
@@ -4,6 +4,7 @@ import sys, traceback
 import sys, time
 import os
 import csv
+import requests
 
 
 
@@ -310,5 +311,67 @@ En ambos casos puede usar gid como query parameter o proveer un custom request h
 gid: HTTP
 _ID
 '''
+#POST
+def Post(string):
+    urlPost="https://reqres.in/api/users"#Metodo 1 Si desea que la url este ingresada pero solo se desea cambiar una vez
 
-print(diccionarioMaestro)
+    #urlPost=input("Ingrese la url con la desea utilizar el metodo POST:\n  ")#Metodo 2 Si desea que la url se ingrese
+
+    params = {'gid':gid}
+    #Se ingresa el gid que es el valor
+
+    payload={'FirstName': 'Michael', 'LastName': 'Kirk', 'phone': '123123'}
+    #la información (data) para subir
+
+    postResponse=requests.post(urlPost, params = params, json=payload)
+    dataPost=postResponse.json()
+    print(postResponse)
+    #imprime el status code
+    print(dataPost)
+    #imprime la data 
+
+def Get(string):
+    urlGet="http://demo7862839.mockable.io/contacts?gid=100"#Metodo 1 Si desea que la url este ingresada pero solo se desea cambiar una vez
+
+    #urlGet=input("Ingrese la url con la desea utilizar el metodo GET:\n  ")#Metodo 2 Si desea que la url se ingrese
+
+    params = {'gid':gid}
+    #Se ingresa el gid que es el valor
+
+    getResponse=requests.get(urlGet, params = params)
+    dataGet=getResponse.json()
+    print(getResponse)
+    #imprime el status code
+    print(dataGet)
+    #imprime la data 
+
+
+def Menu():
+    print("Menu Fase 6:")
+    print("Opción#1 POST")
+    print("Opción#2 GET\n")
+
+def Opciones(OpcionMenu=0):
+	Opcion = int(input("Escoja una opción:\n"))
+	return Opcion
+MenuPrincipal = Menu()
+
+OpcionMenu = Opciones()
+#POST
+
+if (OpcionMenu == 1):
+        gid = input(str("Ingrese el gid para POST:\n"))
+        Post(gid)
+#GET
+elif (OpcionMenu ==2):
+        gid = input(str("Ingresa el gid para GET\n"))
+        Get(gid)
+
+
+
+
+
+
+
+
+#print(diccionarioMaestro)
