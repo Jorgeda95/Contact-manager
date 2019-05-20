efe225877add9fca49c81acd7bce1a6c16b2c586
DavidCorzo <davidcorzo@ufm.edu>
2019-05-19 22:37:26 -0600

    Por si la cago

--- pruebasDAVIDCORZO.py
+++ pruebasDAVIDCORZO.py
@@ -1,4 +1,5 @@
-from funcionesContactManager import *
+# from funcionesContactManager import *
+import requests
 
 
 
@@ -6,14 +7,43 @@ favoriteContactDelete = 'davidcorzo'
 
 favorites = {'davidcorzo': {'nombre': 'David', 'apellido': 'Corzo', 'telefono': '30177050'},'stevenwilson':{'nombre':'Steven','apellido':'Wilson','telefono': '12121212'}}
 diccinario = {}
-diccionarioMaestro = {'davidcorzo':{'nombre':'David','apellido':'Corzo','telefono':'30177050'}, 'favorites': {}}
+diccionarioMaestro = {}
+# 'davidcorzo':{'nombre':'David','apellido':'Corzo','telefono':'30177050'}, 'favorites': {}
 # 'davidcorzo':{'nombre':'David','apellido':'Corzo','telefono':'30177050'}, 'favorites': {'davidcorzo': {'nombre': 'David', 'apellido': 'Corzo', 'telefono': '30177050'}}
 # removeFromFavorites11(favoriteContactDelete,favorites,diccionarioMaestro)
 
+def get14(gid,urlGet,diccionarioMaestro):
+    """Recibe contactos de un URL y los ingresa al diccionario"""
+    #urlGet="http://demo7862839.mockable.io/contacts?gid=100"#Metodo 1 Si desea que la url este ingresada pero solo se desea cambiar una vez
 
+    
+    params = {'gid':gid}
+    #Se ingresa el gid que es el valor
 
+    getResponse=requests.get(urlGet, params = params)
+    dataGet=getResponse.json()
+    # print(getResponse)
+    #imprime el status code
+    # print(dataGet)
+    #imprime la data
+    diccionarioMaestro = dataGet
+    return diccionarioMaestro
 
+gid=100
+urlGet = "http://demo7862839.mockable.io/contacts?gid=100"
 
-newFileName = str(input('Ingrese el nombre de su archivo: (No olvide ponerle extensión) → '))
-exportacionDeLaDataAUnArchivoNuevo16(diccionarioMaestro,favorites,newFileName)
+diccionarioMaestrodeGet = get14(gid,urlGet,diccionarioMaestro)
+def procesarGET(diccionarioMaestrodeGet):
+    # iteration = 0
+    stringize = str(diccionarioMaestrodeGet)
+    a = stringize.replace('FirstName','nombre')
+    b = a.replace('LastName','apellido')
+    c = b.replace('Phone','telefono')
+    return c
 
+compatibleDictionaryL = procesarGET(diccionarioMaestrodeGet)
+
+# def organizar(compatibleDictionary):
+#     iteration = 0
+#     for n in compatibleDictionaryL:
+#         compatibleDictionary[iteration]
